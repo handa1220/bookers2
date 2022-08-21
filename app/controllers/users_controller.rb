4a849f0c7a08f18c9764b78512cc1fc3cd6c481c
handa1220 <108671890+handa1220@users.noreply.github.com>
2022-08-21 13:11:50 +0000

    [Finish]ファイルの準備、ファイルの設定

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,16 @@
+class UsersController < ApplicationController
+  
+  def edit
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def update 
+  end
+
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: "homes#top"
+  devise_for :users
+
+   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+   resources :books, only: [:create, :index, :show, :destroy]
+    resources :users, only: [:edit, :index, :show, :update]
+   
+   get 'homes/about' => 'homes#about', as:'about'
+
 end
