5486ef7223552cd4b3fead8940bf5a8d0bdc3e9d
handa1220 <108671890+handa1220@users.noreply.github.com>
2022-08-22 14:12:08 +0000

    [Finish]アイコン以外のヘッダー作成

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
    resources :books, only: [:create, :index, :show, :destroy]
     resources :users, only: [:edit, :index, :show, :update]
    
-   get 'homes/about' => 'homes#about', as:'about'
+   get 'home/about' => 'homes#about', as:'about'
 
 end
