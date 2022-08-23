28a2c3103234d1e869ef95f3152e5475b7e055a7
handa1220 <108671890+handa1220@users.noreply.github.com>
2022-08-23 13:47:48 +0000

    [Finish]08/23

--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
    # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
    resources :books, only: [:create, :index, :show, :destroy]
-    resources :users, only: [:edit, :index, :show, :update]
+   resources :users, only: [:edit, :index, :show, :update]
    
    get 'home/about' => 'homes#about', as:'about'
 
