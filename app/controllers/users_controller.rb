6cfba2cbb31407ea7e60a53b4acb65a8591b5956
handa1220 <108671890+handa1220@users.noreply.github.com>
2022-08-27 13:26:57 +0000

    [Finish]userのeditページ、update機能の作成

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
   
   def edit
-    
+    @user = User.find(params[:id])
   end
 
   def index
@@ -16,6 +16,9 @@ class UsersController < ApplicationController
   end
 
   def update 
+    @user = User.find(params[:id])
+    @user.update(user_params)
+    redirect_to user_path(@user.id)
   end
 
   private
