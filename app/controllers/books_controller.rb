8857a402f018dd5c0dafc7059a4a7398b663408c
handa1220 <108671890+handa1220@users.noreply.github.com>
2022-08-27 07:48:09 +0000

    [Finish]index、showページの作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -9,9 +9,18 @@ class BooksController < ApplicationController
   
   def index
     @book = Book.new
+    @books = Book.all
   end
   
   def show
+    @book = Book.new
+    @books = Book.find(params[:id])
+  end
+  
+  def destroy
+    @book = Book.find(params[:id])
+    @book.delete
+    redirect_to books_path
   end
   
   private
