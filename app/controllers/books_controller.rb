a20456ee1fd34ec35e2c5686582393f1da31c28d
handa1220 <108671890+handa1220@users.noreply.github.com>
2022-08-30 13:42:46 +0000

    [Finish]4-25

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -48,5 +48,11 @@ class BooksController < ApplicationController
   def book_params
     params.require(:book).permit(:title, :body)
   end
-
+  
+  def correct_user
+    @book = Book.find(params[:id])
+    unless @book.user == current_user
+    redirect_to books_path
+    end
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,5 +30,12 @@ class UsersController < ApplicationController
   def user_params
     params.require(:user).permit(:name, :profile_image, :introduction)
   end
+  
+  def correct_user
+    @user = User.find(params[:id])
+    unless @user == current_user
+    redirect_to user_path(current_user)
+    end
+  end
 
 end
